3f60c7b610cbee3f658acefe46ce3e9a2c4e2363
kou72 <suzukiko7273@gmail.com>
2024-04-14 18:46:32 +0900

    App.vueファイルを修正し、Vueコンポーネント内の変更点を解析
    front/src/App.vue
    - vueからonMountedとrefをインポートする行を削除し、onMountedのみをインポートするよう修正
    - CenterとPinというインターフェースを定義し、centerとpinsをそれぞれの型に合わせて修正
    - centerとpinsの初期化をrefから通常の変数に変更
    - pinをpinsに変数名を修正
    - Loaderのインスタンスを生成するloader変数を削除し、mapMount関数を定義してcenterとpinsを渡すように修正
    - centerの値をcenter.valueからcenterに変更
    - pin.valueをpinsに変更し、forループ内でpinsを使うよう修正
    - mapMount関数を定義し、centerとpinsを渡して呼び出すよう修正
    - CSSで全体のmarginとpaddingをリセットするスタイルを追加

--- front/src/App.vue
+++ front/src/App.vue
@@ -1,9 +1,20 @@
 <script setup lang="ts">
-import { onMounted, ref } from "vue";
+import { onMounted } from "vue";
 import { Loader } from "@googlemaps/js-api-loader";
 
-const center = ref({ lat: 35.6812362, lng: 139.7645445 });
-const pin = ref([
+interface Center {
+  lat: number;
+  lng: number;
+}
+
+interface Pin {
+  name: string;
+  group: string;
+  position: { lat: number; lng: number };
+}
+
+const center: Center = { lat: 35.6812362, lng: 139.7645445 };
+const pins: Pin[] = [
   {
     name: "東京駅",
     group: "お気に入り",
@@ -24,27 +35,31 @@ const pin = ref([
     group: "お気に入り",
     position: { lat: 35.729799, lng: 139.710052 },
   },
-]);
+];
 
 const loader = new Loader({
   apiKey: process.env.VUE_APP_MAPS_API_KEY,
   version: "weekly",
 });
 
-onMounted(async () => {
+const mapMount = async (center: Center, pins: Pin[]) => {
   const google = await loader.load();
   const mapElement = document.getElementById("map") as HTMLElement;
   const map = new google.maps.Map(mapElement, {
-    center: center.value,
+    center: center,
     zoom: 12,
   });
 
-  for (let i = 0; i < pin.value.length; i++) {
+  for (let pin of pins) {
     new google.maps.Marker({
-      position: pin.value[i].position,
+      position: pin.position,
       map: map,
     });
   }
+};
+
+onMounted(async () => {
+  mapMount(center, pins);
 });
 </script>
 
@@ -53,6 +68,10 @@ onMounted(async () => {
 </template>
 
 <style>
+* {
+  margin: 0;
+  padding: 0;
+}
 #map {
   height: 100vh;
   width: 100%;
