3bfbbf92e1dff2d2e1a3d17a68e5e6306c029fab
kou72 <suzukiko7273@gmail.com>
2024-04-14 18:29:07 +0900

    App.vueファイルを修正し、Google Maps APIを使用して複数のピンを表示するように変更
    front/src/App.vue
    - vueからonMountedとrefをインポート
    - centerとpinという複数のピンの情報を持つ変数を定義
    - Google Maps API Loaderを使用して地図を読み込む
    - onMountedフック内で地図を表示し、複数のピンを配置するように修正
    - 以前の地図表示とピン配置のコードを削除し、forループを使用して複数のピンを配置するように変更

--- front/src/App.vue
+++ front/src/App.vue
@@ -1,28 +1,50 @@
 <script setup lang="ts">
-import { onMounted } from "vue";
+import { onMounted, ref } from "vue";
 import { Loader } from "@googlemaps/js-api-loader";
 
+const center = ref({ lat: 35.6812362, lng: 139.7645445 });
+const pin = ref([
+  {
+    name: "東京駅",
+    group: "お気に入り",
+    position: { lat: 35.6812362, lng: 139.7645445 },
+  },
+  {
+    name: "新宿駅",
+    group: "お気に入り",
+    position: { lat: 35.6896342, lng: 139.700645 },
+  },
+  {
+    name: "品川駅",
+    group: "お気に入り",
+    position: { lat: 35.62876, lng: 139.738308 },
+  },
+  {
+    name: "池袋駅",
+    group: "お気に入り",
+    position: { lat: 35.729799, lng: 139.710052 },
+  },
+]);
+
+const loader = new Loader({
+  apiKey: process.env.VUE_APP_MAPS_API_KEY,
+  version: "weekly",
+});
+
 onMounted(async () => {
-  const loader = new Loader({
-    apiKey: process.env.VUE_APP_MAPS_API_KEY,
-    version: "weekly",
+  const google = await loader.load();
+  const mapElement = document.getElementById("map") as HTMLElement;
+  const map = new google.maps.Map(mapElement, {
+    center: center.value,
+    zoom: 12,
   });
 
-  await loader
-    .load()
-    .then((google) => {
-      const mapElement = document.getElementById("map") as HTMLElement;
-      const map = new google.maps.Map(mapElement, {
-        center: { lat: 41.90476224706472, lng: 12.49822074385094 },
-        zoom: 4,
-      });
-      const marker = new google.maps.Marker({
-        position: { lat: 41.90476224706472, lng: 12.49822074385094 },
-        map: map,
-      });
-      console.log(map);
-    })
-    .catch((error) => console.log(error));
+  for (let i = 0; i < pin.value.length; i++) {
+    new google.maps.Marker({
+      position: pin.value[i].position,
+      map: map,
+    });
+  }
 });
 </script>
 
