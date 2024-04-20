eb68b4a2e94999ce64daedf9720b79af3f57af0f
kou72 <suzukiko7273@gmail.com>
2024-04-20 21:03:52 +0900

    GoogleMap.vueファイルにGoogle Maps関連の変更が加えられている
    front/src/components/GoogleMap.vue
    - loaderに"places"ライブラリを追加
    - mapMount関数内で、AdvancedMarkerElementとPinElementをロードする処理を追加
    - mapMount関数内で、google.maps.Markerの代わりにcustomPinを使用した処理を追加
    - mapMount関数内で、AdvancedMarkerElementのcontentにcustomPin.elementを設定する処理を追加

--- front/src/components/GoogleMap.vue
+++ front/src/components/GoogleMap.vue
@@ -25,10 +25,13 @@ const pins = props.pins as Pin[];
 const loader = new Loader({
   apiKey: process.env.VUE_APP_MAPS_API_KEY,
   version: "weekly",
+  libraries: ["places"],
 });
 
 const mapMount = async (center: Center, pins: Pin[]) => {
   const google = await loader.load();
+  const { AdvancedMarkerElement } = await loader.importLibrary("marker");
+  const { PinElement } = await loader.importLibrary("marker");
   const mapElement = document.getElementById("map") as HTMLElement;
   const map = new google.maps.Map(mapElement, {
     center: center,
@@ -37,9 +40,19 @@ const mapMount = async (center: Center, pins: Pin[]) => {
   });
 
   for (let pin of pins) {
-    new google.maps.Marker({
+    const customPin = new PinElement({
+      // blue background
+      background: "#8080FF",
+      // blue glyph
+      glyphColor: "#0000A0",
+      // blue border
+      borderColor: "#0000A0",
+    });
+
+    new AdvancedMarkerElement({
       position: pin.position,
       map: map,
+      content: customPin.element,
     });
   }
 };
