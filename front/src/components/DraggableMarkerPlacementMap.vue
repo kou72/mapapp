b4d8f336f4afc818142443028487b5e140149fad
kou72 <suzukiko7273@gmail.com>
2024-05-03 23:47:25 +0900

    DraggableMarkerPlacementMap.vueとPostPinView.vueを修正し、ピンの色情報を追加、ピンの位置情報を更新するよう変更
    front/src/components/DraggableMarkerPlacementMap.vue
    - defineEmitsをimportして、update:pinイベントを親コンポーネントに通知する
    - updateValue関数を定義し、新しいピンの情報を更新する
    - マップ上でクリックした際に新しいピンの情報を作成し、updateValueで更新する
    
    front/src/views/PostPinView.vue
    - refをimportして、onUpdatedを追加
    - ColorCodeをimportして、pinをrefで定義
    - ピンの初期位置を0,0に設定
    - nameField, latField, lngField, typeField, colorFieldをrefで定義
    - searchInputを検索用の入力フィールドとして定義
    - searchInputの値をnameFieldにコピーし、ピンの情報を更新する
    - onRegister関数内で各フィールドの値とピンの情報をログ出力
    - onUpdatedでピンの情報をログ出力
    - DraggableMarkerPlacementMapコンポーネントにv-modelでpinを双方向バインディング
    - SearchFieldコンポーネントのv-modelをsearchInputに変更
    - nameField, latField, lngField, typeFieldに対応するTextFieldコンポーネントのv-modelを修正

--- front/src/components/DraggableMarkerPlacementMap.vue
+++ front/src/components/DraggableMarkerPlacementMap.vue
@@ -1,14 +1,18 @@
 <script setup lang="ts">
 /*global google*/
 /*eslint no-undef: "error"*/
-import { defineProps, PropType, onMounted, onUpdated } from "vue";
+import { defineProps, PropType, onMounted, onUpdated, defineEmits } from "vue";
 import { Loader } from "@googlemaps/js-api-loader";
-import { Center, Pin } from "@/types/map-interfaces";
+import { Center, Pin, ColorCode } from "@/types/map-interfaces";
 import { customPinsColor } from "@/utils/customPinsColor";
 
 const props = defineProps({
   pin: { type: Object as PropType<Pin>, required: false },
 });
+
+// pinの値が変更されたときに親コンポーネントに通知する
+const emit = defineEmits(["update:pin"]);
+const updateValue = (value: Pin) => emit("update:pin", value);
 
 let map: google.maps.Map;
 let markers: google.maps.marker.AdvancedMarkerElement[] = [];
@@ -37,9 +41,21 @@ const initMap = async () => {
     // 高度なマーカーを使用するためにmapIdの取得が必要
     mapId: process.env.VUE_APP_MAP_ID,
   });
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  map.addListener("click", (e: any) => {
+    const newPin = {
+      id: "",
+      name: "",
+      group: "",
+      color: "red" as ColorCode,
+      position: { lat: e.latLng.lat(), lng: e.latLng.lng() },
+    };
+    console.log(newPin);
+    updateValue(newPin);
+  });
 };
 
-const initPins = async () => {
+const setPin = async () => {
   const { PinElement, AdvancedMarkerElement } = await loadeGoogleMapsLibrary();
   if (!props.pin) return;
   const customPin = new PinElement(customPinsColor(props.pin.color));
@@ -49,17 +65,21 @@ const initPins = async () => {
     content: customPin.element,
     gmpDraggable: true,
   });
-  // マーカーを操作するために保存する。pins[]と2重管理になる
+  // 必要なピンは1つだけなのでmarkers[]も常に1つだけ
+  // 既存のピンがあれば削除する
+  if (markers[0]) {
+    markers[0].map = null;
+    markers.splice(0, 1);
+  }
   markers.push(marker);
 };
 
 onMounted(async () => {
   await initMap();
-  await initPins();
 });
 
 onUpdated(async () => {
-  // await initPins();
+  await setPin();
 });
 </script>
 
--- front/src/views/PostPinView.vue
+++ front/src/views/PostPinView.vue
@@ -1,41 +1,51 @@
 <script setup lang="ts">
-import { ref } from "vue";
-import { Pin } from "@/types/map-interfaces";
+import { ref, onUpdated } from "vue";
+import { ColorCode, Pin } from "@/types/map-interfaces";
 import DraggableMarkerPlacementMap from "@/components/DraggableMarkerPlacementMap.vue";
 import SearchField from "@/components/ui/SearchField.vue";
 import TextField from "@/components/ui/TextField.vue";
 import FlatButton from "@/components/ui/FlatButton.vue";
 import OutlinedButton from "@/components/ui/OutlinedButton.vue";
 import "@mdi/font/css/materialdesignicons.css";
-const pin: Pin = {
+
+const pin = ref<Pin>({
   id: "",
   name: "",
   group: "",
   color: "red",
-  position: { lat: 35.6812362, lng: 139.7645445 },
-};
-
-const inputText = ref("");
+  position: { lat: 0, lng: 0 },
+});
+const searchInput = ref("");
 const loading = ref(false);
-const name = ref("");
-const latitude = ref("");
-const longitude = ref("");
-const type = ref("");
+const nameField = ref("");
+const latField = ref(0);
+const lngField = ref(0);
+const typeField = ref("");
+const colorField = ref<ColorCode>("red");
 
 // eslint-disable-next-line @typescript-eslint/no-explicit-any
 const copyToPinValue = (data: any) => {
-  name.value = inputText.value;
-  inputText.value = "";
+  nameField.value = searchInput.value;
+  searchInput.value = "";
   const location = data.results[0].geometry.location;
-  latitude.value = location.lat;
-  longitude.value = location.lng;
+  latField.value = location.lat;
+  lngField.value = location.lng;
+
+  const newPin = {
+    id: "",
+    name: nameField.value,
+    group: "",
+    color: colorField.value,
+    position: { lat: location.lat, lng: location.lng },
+  };
+  pin.value = newPin;
 };
 
 const search = async () => {
   const url = "https://maps.googleapis.com/maps/api/geocode/json";
   loading.value = true;
   const res = await fetch(
-    `${url}?address=${inputText.value}&key=${process.env.VUE_APP_MAPS_API_KEY}`
+    `${url}?address=${searchInput.value}&key=${process.env.VUE_APP_MAPS_API_KEY}`
   );
   const data = await res.json();
   console.log(data);
@@ -45,35 +55,40 @@ const search = async () => {
 
 const onRegister = () => {
   // 登録ボタンがクリックされたときの処理
-  console.log("Name:", name.value);
-  console.log("Latitude:", latitude.value);
-  console.log("Longitude:", longitude.value);
-  console.log("Type:", type.value);
-  console.log("SearchText:", inputText.value);
+  console.log("Name:", nameField.value);
+  console.log("Latitude:", latField.value);
+  console.log("Longitude:", lngField.value);
+  console.log("Type:", typeField.value);
+  console.log("SearchText:", searchInput.value);
+  console.log("pin:", pin.value);
 };
+
+onUpdated(() => {
+  console.log("Pin:", pin.value);
+});
 </script>
 
 <template>
   <v-row class="ma-2">
     <!-- Googleマップ -->
     <v-col cols="8">
-      <DraggableMarkerPlacementMap :pin="pin" />
+      <DraggableMarkerPlacementMap v-model:pin="pin" />
     </v-col>
     <v-col cols="4" class="mt-4">
       <!-- 検索用テキストフィールド -->
-      <SearchField v-model="inputText" :loading="loading" :func="search" />
+      <SearchField v-model="searchInput" :loading="loading" :func="search" />
       <!-- ピンの要素 -->
       <div class="mx-4 mt-12 mb-16">
-        <TextField label="名前" v-model="name" />
+        <TextField label="名前" v-model="nameField" />
         <v-row class="py-3">
           <v-col cols="6">
-            <TextField label="緯度" v-model="latitude" />
+            <TextField label="緯度" v-model="latField" />
           </v-col>
           <v-col cols="6">
-            <TextField label="経度" v-model="longitude" />
+            <TextField label="経度" v-model="lngField" />
           </v-col>
         </v-row>
-        <TextField label="タイプ" v-model="type" />
+        <TextField label="タイプ" v-model="typeField" />
       </div>
       <!-- ボタン -->
       <v-row justify="end" class="mx-4">
