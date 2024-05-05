75dce4f4ff38a1d6ff3ed41ff6ee5db425a95447
kou72 <suzukiko7273@gmail.com>
2024-05-06 00:27:53 +0900

    PinsList.vueファイルで、グループ化されたピンを表示するように修正
    PinsList.vue
    - groupedPins関数を定義し、ピンをグループ化する処理を追加
    - v-forディレクティブを使ってグループごとにピンを表示するように修正

--- front/src/components/PinsList.vue
+++ front/src/components/PinsList.vue
@@ -5,20 +5,34 @@ import { Pin } from "@/types/map-interfaces";
 const props = defineProps({
   pins: Array as PropType<Pin[]>,
 });
+
+const groupedPins = () => {
+  const groups: { [key: string]: Pin[] } = {};
+  props.pins?.forEach((pin) => {
+    if (!groups[pin.group]) {
+      groups[pin.group] = [];
+    }
+    groups[pin.group].push(pin);
+  });
+  return groups;
+};
 </script>
 
 <template>
   <v-card>
     <v-list>
-      <v-list-item-group>
-        <v-list-item v-for="pin in props.pins" :key="pin.id">
-          <v-list-item-content>
-            {{ pin.group }}
-            {{ pin.name }}
-            緯度：{{ pin.position.lat }} 経度：{{ pin.position.lng }}
-          </v-list-item-content>
-        </v-list-item>
-      </v-list-item-group>
+      <template v-for="(group, groupName) in groupedPins()" :key="groupName">
+        <v-subheader>{{ groupName }}</v-subheader>
+        <v-list-item-group>
+          <v-list-item v-for="pin in group" :key="pin.id">
+            <v-list-item-content>
+              {{ pin.name }}
+              緯度：{{ pin.position!.lat }} 経度：{{ pin.position!.lng }}
+            </v-list-item-content>
+          </v-list-item>
+        </v-list-item-group>
+        <v-divider></v-divider>
+      </template>
     </v-list>
   </v-card>
 </template>
